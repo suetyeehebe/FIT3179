e098df7658c5d2a671c344c01ffe9ddbd310aa4f
suetyeehebe <140065190+suetyeehebe@users.noreply.github.com>
2025-09-30 15:39:37 +0800

    created the vega-lite json file, html file, JavaScript file

---
+++ vega_lite_vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "js/crude_death_rate_map.vg.json";
+
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
